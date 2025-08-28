0daa35b0990e1ddadfbd5f65db032ee01c7d770d
Zack <168221270+Zval321@users.noreply.github.com>
2025-08-28 13:18:23 -0500

    updates

---
+++ spotbeat/src/screens/Header/Header_temp.tsx
@@ -0,0 +1,47 @@
+import './Header.css'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { Modal } from '../../components/Modal/Modal'
+
+
+
+export function Header() {
+  const navigate = useNavigate()
+  // const [location, setLocation] = useState('')
+  const [modalVisible, setModalVisible] = useState(false)
+
+
+  const handleOpenModal = () => {
+    console.log('openingModal')
+    setModalVisible(true)
+  }
+
+
+  return (
+    <div className="header-container">
+      <div className="header-image-container">
+        <img src="/logo.png" alt="SpotBeat Logo" />
+      </div>
+
+      <div className="header-title-container">
+        <button
+          onClick={() => navigate('/')}
+          style={{ backgroundColor: '#fcfcfc', border: 'none', outline: 'none', cursor: 'pointer' }}
+        >
+          <h1>SpotBeat</h1>
+        </button>
+      </div>
+
+      <div className='location-modal-button-container'>
+        <button onClick={handleOpenModal} className='button-open-location-modal'>Location
+        </button>
+      </div>
+
+      <Modal
+        show={modalVisible}
+        onClose={() => setModalVisible(false)}
+      />
+
+    </div>
+  )
+}
