957dd0ef0840cfb152f0f6db7f2707f6fcb0d4e1
Zack <168221270+Zval321@users.noreply.github.com>
2025-09-03 21:09:47 -0500

    added the post route to backend

--- spotbeat-backend/src/controllers/likesController.ts
+++ spotbeat-backend/src/controllers/likesController.ts
@@ -1,9 +1,38 @@
 import type { Request, Response } from 'express';
 import { supabase } from '../utils/supabase.js';
 
+
+// get all likes - For Testing Purposes
 export const getAllLikes = async (_req: Request, res: Response) => {
   const { data, error } = await supabase.from('likes').select('*');
 
   if (error) return res.status(500).json({ error: error.message });
   res.status(200).json(data);
+}
+
+//Get likes by user ID
+
+export const getLikesByUser = async (_req: Request, res: Response) => {
+  const userId = _req.params.userId
+
+  const { data, error } = await supabase.from('likes').select('*').eq('user_id', userId);
+
+  if (error) return res.status(500).json({ error: error.message })
+
+  res.status(200).json(data)
+}
+
+// Add a like
+
+export const addLike = async (_req: Request, res: Response) => {
+  const { user_id, event_id } = _req.body;
+
+  if (!user_id || !event_id) {
+    return res.status(400).json({ error: 'user_id and event_id are required' });
+  }
+  const { data, error } = await supabase.from('likes').insert({ user_id, event_id }).select().single();
+
+  if (error) return res.status(500).json({ error: error.message })
+
+  res.status(201).json(data)
 }
